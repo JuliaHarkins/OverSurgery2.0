ff4ca884336f4cd0b7a0ac98539dfe63a4003f8b
Lewis Barnes <362490@edu.cwa.ac.uk>
2017-11-15 14:30:17 +0000

    0

--- oversurgery2/mainclasses/appointment.cs
+++ oversurgery2/mainclasses/appointment.cs
@@ -21,8 +21,8 @@ namespace OverSurgery2
         private string m_notes;
         private int m_medicalStaffID;
         private int m_patientID;
-        private int m_appDate;
-        private int m_appTime;
+        private DateTime m_appDate;
+        private DateTime m_appTime;
         private int m_appointmentID;
         private bool m_appAttend;
         private string m_forenameDisplay;
@@ -33,8 +33,8 @@ namespace OverSurgery2
         public string Notes { get { return m_notes; } set { m_notes = value; } } //Appointment.Notes;  
         public int MedicalStaffID { get { return m_medicalStaffID; } set { m_medicalStaffID = value; } }
         public int PatientID { get { return m_patientID; } set { m_patientID = value; } }        
-        public int AppDate { get { return m_appDate; } set { m_appDate = value; } }
-        public int AppTime { get { return m_appTime; } set { m_appTime = value; } }                                                                               // Stores the date and time of the appointment
+        public DateTime AppDate { get { return m_appDate; } set { m_appDate = value; } }
+        public DateTime AppTime { get { return m_appTime; } set { m_appTime = value; } }                                                                               // Stores the date and time of the appointment
         public int AppointmentID { get { return m_appointmentID; } set { m_appointmentID = value; } }
         public bool AppAttend { get { return m_appAttend; } set { m_appAttend = value; } }
         public string ForeNameDisplay { get { return m_forenameDisplay; } }
@@ -50,10 +50,10 @@ namespace OverSurgery2
         {
             ml = MetaLayer.Instance();
             AppointmentID = Convert.ToInt16(p_appValues["AppID"]);
-            MedicalStaffID = Convert.ToInt16(p_appValues["RegisteredDoctorID"]);
+            MedicalStaffID = Convert.ToInt16(p_appValues["MedStaffID"]);
             PatientID = Convert.ToInt16(p_appValues["PatientID"]);
-            AppDate = Convert.ToInt16(p_appValues["Date"]);
-            AppTime = Convert.ToInt16(p_appValues["Time"]);
+            AppDate = Convert.ToDateTime(p_appValues["Date"]);
+            AppTime = DateTime.Parse(p_appValues["Time"].ToString());
             Notes = Convert.ToString(p_appValues["Notes"]);
             AppAttend = Convert.ToBoolean(p_appValues["Attend"]);
             
