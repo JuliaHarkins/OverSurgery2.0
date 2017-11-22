5df613f7ec59bd8470be9f5ee830a0c0cd8ac97a
Lewis Barnes <362490@edu.cwa.ac.uk>
2017-11-22 15:36:19 +0000

    Making NewPatientForm

---
+++ OverSurgery2/UserInterface/NewPatientForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace OverSurgery2
+{
+    partial class NewPatientForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Forename = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labe = new System.Windows.Forms.Label();
+            this.lbl_Gender = new System.Windows.Forms.Label();
+            this.lbl_Surname = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_Forename
+            // 
+            this.lbl_Forename.AutoSize = true;
+            this.lbl_Forename.Location = new System.Drawing.Point(12, 9);
+            this.lbl_Forename.Name = "lbl_Forename";
+            this.lbl_Forename.Size = new System.Drawing.Size(86, 20);
+            this.lbl_Forename.TabIndex = 0;
+            this.lbl_Forename.Text = "Forename:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Forename:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Forename:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Forename:";
+            // 
+            // labe
+            // 
+            this.labe.AutoSize = true;
+            this.labe.Location = new System.Drawing.Point(12, 69);
+            this.labe.Name = "labe";
+            this.labe.Size = new System.Drawing.Size(106, 20);
+            this.labe.TabIndex = 4;
+            this.labe.Text = "Date Of Birth:";
+            // 
+            // lbl_Gender
+            // 
+            this.lbl_Gender.AutoSize = true;
+            this.lbl_Gender.Location = new System.Drawing.Point(12, 49);
+            this.lbl_Gender.Name = "lbl_Gender";
+            this.lbl_Gender.Size = new System.Drawing.Size(67, 20);
+            this.lbl_Gender.TabIndex = 5;
+            this.lbl_Gender.Text = "Gender:";
+            // 
+            // lbl_Surname
+            // 
+            this.lbl_Surname.AutoSize = true;
+            this.lbl_Surname.Location = new System.Drawing.Point(12, 29);
+            this.lbl_Surname.Name = "lbl_Surname";
+            this.lbl_Surname.Size = new System.Drawing.Size(74, 20);
+            this.lbl_Surname.TabIndex = 6;
+            this.lbl_Surname.Text = "Surname";
+            // 
+            // NewPatientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 402);
+            this.Controls.Add(this.lbl_Surname);
+            this.Controls.Add(this.lbl_Gender);
+            this.Controls.Add(this.labe);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_Forename);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "NewPatientForm";
+            this.Text = "NewPatientForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Forename;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labe;
+        private System.Windows.Forms.Label lbl_Gender;
+        private System.Windows.Forms.Label lbl_Surname;
+    }
+}
