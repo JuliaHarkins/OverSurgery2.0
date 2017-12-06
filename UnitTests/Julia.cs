c0e5d17c23afdc22d4aae04f28bb9e9b595114c7
Julia Harkins <398905@edu.cwa.ac.uk>
2017-12-06 15:53:06 +0000

    created the first test

--- UnitTests/Julia.cs
+++ UnitTests/Julia.cs
@@ -11,6 +11,13 @@ namespace UnitTests
     [TestClass]
     public class Julia
     {
-
+        MetaLayer ml = MetaLayer.Instance();
+        List<Medication> medicatinoList = new List<Medication>();
+        
+        public void Querries()
+        {
+            medicatinoList = ml.getMedicationOnMedStaffID(3);
+            Assert.AreEqual(medicatinoList[0].Name,"Asprin");
+        }
     }
 }
